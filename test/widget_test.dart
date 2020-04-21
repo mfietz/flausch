8f67fc997d3e2ece3429e6e8bae63eb1fd53ef64
Martin Fietz <martin.fietz@gmail.com>
2020-04-21 20:26:08 +0200

    Delete test

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flausch/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-  });
-}
