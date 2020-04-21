a007edf501028e147eeaee2963d70abe3a1ccdfc
Martin Fietz <martin.fietz@gmail.com>
2020-04-21 19:52:25 +0200

    Cleanup models

--- lib/data/reddit_response_models.dart
+++ lib/data/reddit_response_models.dart
@@ -46,22 +46,14 @@ class Child {
 
 class ChildData {
   String title;
-  int downs;
-  int ups;
   int thumbnailHeight;
   int thumbnailWidth;
   String name;
-  Media secureMedia;
-  bool isRedditMediaDomain;
-  dynamic category;
   int score;
   String thumbnail;
   PostHint postHint;
   Domain domain;
-  bool over18;
   Preview preview;
-  bool mediaOnly;
-  bool canGild;
   String id;
   String author;
   String permalink;
@@ -72,21 +64,14 @@ class ChildData {
 
   ChildData({
     this.title,
-    this.downs,
-    this.ups,
     this.thumbnailHeight,
     this.name,
     this.thumbnailWidth,
-    this.secureMedia,
-    this.category,
     this.score,
     this.thumbnail,
     this.postHint,
     this.domain,
-    this.over18,
     this.preview,
-    this.mediaOnly,
-    this.canGild,
     this.id,
     this.author,
     this.permalink,
@@ -98,24 +83,15 @@ class ChildData {
 
   factory ChildData.fromJson(Map<String, dynamic> json) => ChildData(
         title: json["title"],
-        downs: json["downs"],
-        ups: json["ups"],
         thumbnailHeight: json["thumbnail_height"],
         name: json["name"],
         thumbnailWidth: json["thumbnail_width"],
-        secureMedia: json["secure_media"] == null
-            ? null
-            : Media.fromJson(json["secure_media"]),
-        category: json["category"],
         score: json["score"],
         thumbnail: json["thumbnail"],
         postHint: postHintValues.map[json["post_hint"]],
         domain: domainValues.map[json["domain"]],
-        over18: json["over_18"],
         preview:
             json["preview"] == null ? null : Preview.fromJson(json["preview"]),
-        mediaOnly: json["media_only"],
-        canGild: json["can_gild"],
         id: json["id"],
         author: json["author"],
         permalink: json["permalink"],
@@ -155,12 +131,10 @@ final domainValues = EnumValues({
 
 class Media {
   RedditVideo redditVideo;
-  Oembed oembed;
   Domain type;
 
   Media({
     this.redditVideo,
-    this.oembed,
     this.type,
   });
 
@@ -168,77 +142,30 @@ class Media {
         redditVideo: json["reddit_video"] == null
             ? null
             : RedditVideo.fromJson(json["reddit_video"]),
-        oembed: json["oembed"] == null ? null : Oembed.fromJson(json["oembed"]),
         type: json["type"] == null ? null : domainValues.map[json["type"]],
       );
 }
 
-class Oembed {
-  String description;
-  String title;
-  int height;
-  int width;
-  String html;
-  int thumbnailWidth;
-  String thumbnailUrl;
-  String type;
-  int thumbnailHeight;
-  String url;
-
-  Oembed({
-    this.description,
-    this.title,
-    this.height,
-    this.width,
-    this.thumbnailWidth,
-    this.thumbnailUrl,
-    this.type,
-    this.thumbnailHeight,
-    this.url,
-  });
-
-  factory Oembed.fromJson(Map<String, dynamic> json) => Oembed(
-        description: json["description"],
-        title: json["title"],
-        height: json["height"],
-        width: json["width"],
-        thumbnailWidth: json["thumbnail_width"],
-        thumbnailUrl: json["thumbnail_url"],
-        type: json["type"],
-        thumbnailHeight: json["thumbnail_height"],
-        url: json["url"] == null ? null : json["url"],
-      );
-}
-
 class RedditVideo {
-  String fallbackUrl;
   int height;
   int width;
-  String scrubberMediaUrl;
   String dashUrl;
   int duration;
-  String hlsUrl;
   bool isGif;
 
   RedditVideo({
-    this.fallbackUrl,
     this.height,
     this.width,
-    this.scrubberMediaUrl,
     this.dashUrl,
     this.duration,
-    this.hlsUrl,
     this.isGif,
   });
 
   factory RedditVideo.fromJson(Map<String, dynamic> json) => RedditVideo(
-        fallbackUrl: json["fallback_url"],
         height: json["height"],
         width: json["width"],
-        scrubberMediaUrl: json["scrubber_media_url"],
         dashUrl: json["dash_url"],
         duration: json["duration"],
-        hlsUrl: json["hls_url"],
         isGif: json["is_gif"],
       );
 }
@@ -276,13 +203,11 @@ class Preview {
 class RedditImage {
   ResizedIcon source;
   List<ResizedIcon> resolutions;
-  Variants variants;
   String id;
 
   RedditImage({
     this.source,
     this.resolutions,
-    this.variants,
     this.id,
   });
 
@@ -290,39 +215,7 @@ class RedditImage {
         source: ResizedIcon.fromJson(json["source"]),
         resolutions: List<ResizedIcon>.from(
             json["resolutions"].map((x) => ResizedIcon.fromJson(x))),
-        variants: Variants.fromJson(json["variants"]),
         id: json["id"],
-      );
-}
-
-class Variants {
-  Gif gif;
-  Gif mp4;
-
-  Variants({
-    this.gif,
-    this.mp4,
-  });
-
-  factory Variants.fromJson(Map<String, dynamic> json) => Variants(
-        gif: json["gif"] == null ? null : Gif.fromJson(json["gif"]),
-        mp4: json["mp4"] == null ? null : Gif.fromJson(json["mp4"]),
-      );
-}
-
-class Gif {
-  ResizedIcon source;
-  List<ResizedIcon> resolutions;
-
-  Gif({
-    this.source,
-    this.resolutions,
-  });
-
-  factory Gif.fromJson(Map<String, dynamic> json) => Gif(
-        source: ResizedIcon.fromJson(json["source"]),
-        resolutions: List<ResizedIcon>.from(
-            json["resolutions"].map((x) => ResizedIcon.fromJson(x))),
       );
 }
 
